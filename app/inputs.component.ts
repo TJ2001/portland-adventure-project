8117626a3d916f9acf9b0c6050220aa7421afdfa
Callan McNulty <jabberwocky222@gmail.com>
2016-09-01 15:27:03 -0400

    Add parameters to foursquare and weather apis

--- app/inputs.component.ts
+++ app/inputs.component.ts
@@ -18,7 +18,20 @@ import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES,ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'an
       <option value="hiking">hiking</option>
       <option value="mountain biking">mountain biking</option>
       <option value="camping">camping</option>
+      <option value="food">food</option>
+      <option value="drinks">drinks</option>
+      <option value="coffee">coffee</option>
+      <option value="shops">shops</option>
+      <option value="sights">sights</option>
+      <option value="outdoors">outdoors</option>
+      <option value="arts">arts</option>
     </select>
+      <div class="form-group row">
+        <label for="example-text-input" class="col-xs-2 col-form-label">Zip Code: </label>
+        <div class="col-xs-10">
+        <input class="form-control" #zip>
+        </div>
+      </div>
       <div class="form-group row">
         <label for="example-text-input" class="col-xs-2 col-form-label">City: </label>
         <div class="col-xs-10">
@@ -37,14 +50,20 @@ import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES,ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'an
         <input class="form-control" #country>
         </div>
       </div>
-      <button (click)="addInputs(city, state, country, activity)" class="btn btn-danger btn-lg">Add</button>
+      <button (click)="addInputs(city, state, country, activity, zip)" class="btn btn-danger btn-lg">Add</button>
     </div>
-    <div *ngFor="#place of response.places">
+    <div *ngFor="#place of responseTrails.places">
       <div *ngFor="#activity of place.activities">
         <h4>{{activity.name}}</h4>
         <h5>{{activity.url}}</h5>
         <img src="{{activity.thumbnail}}" alt="picture of location">
       </div>
+    </div>
+    <div *ngFor="#venue of responseFourSquare.response.venues">
+      <h4>{{venue.name}}</h4>
+    </div>
+    <div *ngFor="#day of responseWeather.query.results.channel.item.forecast">
+      <h4>{{day.date}}</h4>
     </div>
     <sebm-google-map [latitude]="lat" [longitude]="lng" [zoom]="zoom">
   </sebm-google-map>
@@ -53,42 +72,51 @@ import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES,ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'an
 
 
 export class InputFormComponent {
-  public response: any;
+  public responseTrails: any;
+  public responseFourSquare: any;
+  public responseWeather: any;
   public quest;
   lat: number;
   lng: number;
   zoom: number = 10
   constructor(private _firebaseService: FirebaseService,  private TrailService: TrailService, private WeatherService: WeatherService, private FoursquareService: FoursquareService, private GeocodeService: GeocodeService) {
-    this.response = {places: []};
+    this.responseTrails = {places: []};
+    this.responseFourSquare = {response: {venues: []}};
+    this.responseWeather = {query: {results: { channel: { item:{ forecast: []}}}}};
   }
 
-  addInputs(city: HTMLInputElement, state: HTMLInputElement, country: HTMLInputElement, activity: HTMLSelectElement) {
-    var newQuest = new Quest(city.value, state.value, country.value, activity.value);
+  addInputs(city: HTMLInputElement, state: HTMLInputElement, country: HTMLInputElement, activity: HTMLSelectElement, zip: HTMLInputElement) {
+    var newQuest = new Quest(city.value, state.value, country.value, activity.value, zip.value);
     this.quest = newQuest;
+    zip.value = "";
     city.value = "";
     state.value = "";
     country.value = "";
-    this.TrailService.getTrail(this.quest.city, this.quest.state, this.quest.country, this.quest.activity)
+    this.WeatherService.getWeather(this.quest.city)
     .subscribe(
-      data => this.response = data,
+      data => this.responseWeather = data,
       error => console.log(error)
     );
-      this.WeatherService.getWeather()
+    if(this.quest.activity==="hiking"||this.quest.activity==="camping"||this.quest.activity==="mountain biking") {
+      this.TrailService.getTrail(this.quest.city, this.quest.state, this.quest.country, this.quest.activity)
       .subscribe(
-        data => console.log(data),
+        data => this.responseTrails = data,
         error => console.log(error)
       );
-      this.FoursquareService.getFoursquare()
+    } else {
+    //if(this.quest.activity==="food"||this.quest.activity==="drinks"||this.quest.activity==="coffee"||this.quest.activity==="shops"||this.quest.activity==="arts"||this.quest.activity==="outdoors"||this.quest.activity==="sights") {
+      this.FoursquareService.getFoursquare(this.quest.zip, this.quest.activity)
       .subscribe(
-        data => console.log(data),
-        error => console.log(error)
-      );
-      this.GeocodeService.getGeocode()
-      .subscribe(
-        data => {console.log(data); this.lat =  data.results[0].geometry.location.lat; this.lng =  data.results[0].geometry.location.lng},
+        data => this.responseFourSquare = data,
         error => console.log(error)
       );
     }
+    this.GeocodeService.getGeocode()
+    .subscribe(
+      data => {console.log(data); this.lat =  data.results[0].geometry.location.lat; this.lng =  data.results[0].geometry.location.lng},
+      error => console.log(error)
+    );
+  }
 }
 
 
--- app/quest.model.ts
+++ app/quest.model.ts
@@ -1,5 +1,5 @@
 export class Quest {
-  constructor(public city: string, public state: string, public country: string, public activity: string) {
+  constructor(public city: string, public state: string, public country: string, public activity: string, public zip: string) {
 
   }
 }
