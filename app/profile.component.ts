b7410119e18ec991679a7e03762b58f8465d8c9b
BrianHGrant <bhgrant8@gmail.com>
2016-09-01 23:03:37 -0700

    add pip to format date

--- app/profile.component.ts
+++ app/profile.component.ts
@@ -1,10 +1,14 @@
 import { Component } from 'angular2/core';
+import { Pipe, PipeTransform } from 'angular2/core';
 import { Auth } from './auth.service';
 import { FirebaseService } from './firebase.service';
-
+import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
+import { DateFormatPipe} from './date-format.pipe';
+declare var moment: any;
 @Component({
   styleUrls: ['app/profile.css'],
   providers: [ Auth, FirebaseService ],
+  pipes: [DateFormatPipe],
   template:`
   <div class="main-content">
     <h2>{{auth.userProfile.nickname}}'s Profile</h2>
@@ -12,7 +16,7 @@ import { FirebaseService } from './firebase.service';
       <div class="col-md-6 user-info">
         <h3>Your Score: {{auth.userProfile.user_metadata.score}}</h3>
         <h3>You have been a member since:</h3>
-        <h4 id="date">{{auth.userProfile.created_at}}</h4>
+        <h4 id="date">{{auth.userProfile.created_at | dateFormat}}</h4>
         <h3>You have reached the level of:</h3>
         <div *ngIf="auth.userProfile.user_metadata.score < 1000">
           <img class="avatar-img" src="/resources/img/knight-avatars/page.png" alt="page image"/>
@@ -61,11 +65,14 @@ import { FirebaseService } from './firebase.service';
 export class ProfileComponent {
   responseFirebase: any;
   firebaseKeys: Array<string>;
-  constructor(private auth: Auth, private _firebaseService: FirebaseService) {
+  constructor(private auth: Auth, private _firebaseService: FirebaseService, private router: Router) {
     this._firebaseService.getAllQuests()
       .subscribe(
         data => {this.responseFirebase = data, this.firebaseKeys = Object.keys(data)},
         error => console.log(error)
       );
+  }
+  goToQuest(id) {
+    this.router.navigate( ['Quest', { quest_id: id }] );
   }
 }
